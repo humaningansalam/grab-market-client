ffbabf56d4438c8407b073a4721335f5ac609d91
sh9634 <sanghun9634@gmail.com>
2022-07-18 15:34:33 +0000

    project init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MainPageComponent from "./main/index.js";
+
+function App() {
+  return (
+    <div>
+      <MainPageComponent />
+    </div>
+  );
+}
+
+export default App;
