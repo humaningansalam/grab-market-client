1846dd1be1621caab0070c547de768c6a2d107c5
sh9634 <sanghun9634@gmail.com>
2022-07-20 02:29:17 +0000

    antd add

--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,43 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get("")
+      .then(function (result) {
+        setProduct(result.data);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-  return <h1>상품관련 {id} 페이지</h1>;
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다....</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">2021년 4월 3일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
