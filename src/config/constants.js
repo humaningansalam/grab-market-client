a44765791c16335085ca0eff39b2986f74768dcd
sh9634 <sanghun9634@gmail.com>
2022-07-22 15:58:56 +0000

    docker server ready2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://home9634.duckdns.org:21011";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://home9634.duckdns.org:21011"
+    : "http://192.168.0.100:21011";
